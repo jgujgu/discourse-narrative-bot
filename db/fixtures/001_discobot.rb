d513a560441751ab95b18ca77ba94104f2a05340
jgu <jgu@jeffreygu.com>
2017-01-25 16:51:34 -0700

    default to mebot

--- db/fixtures/001_discobot.rb
+++ db/fixtures/001_discobot.rb
@@ -1,4 +1,4 @@
-discobot_username ='discobot'
+discobot_username ='mebot'
 user = User.find_by("id <> -2 and username_lower = '#{discobot_username}'")
 
 if user
@@ -36,7 +36,7 @@ Group.user_trust_level_change!(-2, TrustLevel[4])
 
 # TODO Pull the user avatar from that thread for now. In the future, pull it from a local file or from some central discobot repo.
 UserAvatar.import_url_for_user(
-  "https://cdn.discourse.org/dev/uploads/default/original/2X/e/edb63d57a720838a7ce6a68f02ba4618787f2299.png",
+  "https://00e9e64bac392ad26b23b1918fda8fd506abfaa75cba33fa04-apidata.googleusercontent.com/download/storage/v1/b/220d5b896cce2bed16141ec4c7fa4a85f5229606/o/mebot.png?qk=AD5uMEt8eF-Un-cnQJZr3kMhKTA2yN8K4NQkQ08ZdMMgK5lT_YcXiqTG2lPPhLcemAAecbh5kHc7-lrtY_maU6c32tnmD-OcMRe-cNnoLKKTtXY1GGDjIpNSpfUhQHfuPBhDg3WZh2tia3Y_Eebh7wdargc2pDIKftb_4ShyZf1DECRNaxYZqtWw16Kleou78J_5eagdtr7tu01FoIMEfshVqQoPm3vMs2Av_ValwMbLp_Ld8NcK4PFG_1cdCsE-nbAXndrI8YFUKZaFtkTeKEg7g6q4wDqHzwBYBkwVAPFTePM2xP9NSE0ZTJYQQhcNFGLMHXyiZlNpFVqvrSasoQFoVy9MMIY-Hn4FeXGzVigmymcIcI0C_NIf3TYTXamRT7YBs12nijb9OQhZXFFawxEEHrv67pdZW5WkQ-4totcDWtdjgDaAyAgIyD9h-L1BPGl1LYKqhZKoDF0rhWgvL8DklqhiuitrKf__djaLLa7zOjHskRR_ff_CHkvTK_QY1WKJpnl8xRaLemtWRirf9bCOiiw78pe8LYcJFBWtW1bbSM1Ixj7iMkYmYXm62fFAHGYDXlfJb7gQCHJqV7VnyRx4_HuXs8pnMFUy9_52asDQ8zOrVrEdBwpeWTdR3ZxZlKh9gh6PmJ6nP3HHnGYBI-1cAM-NMp45WtNPTOJFMa4O3oiyeppSQrXe8lTb3EOTn6SfVK1amDEX7sVlH2IYuAyvzPyyBeCGf0TKEZHcOSARXN0R9hl8EHRdt8RkRU46wTGT5QwXMmVEniQFMWXbht-lsUfK7UX4ZiKnxM53dkiYpaJ8QzvATXg",
   User.find_by(username: discobot_username),
   override_gravatar: true
 )
